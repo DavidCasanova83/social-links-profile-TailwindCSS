d8230b42a767cef3d9d3e85a9c920be2a16302d1
David Casanova <casanova.83130@gmail.com>
2024-06-10 18:31:48 +0200

    git init

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'customBlack': '#141414',
+        'customGray': '#1F1F1F',
+        'customGrayLight': '#333333',
+
+
+      },
+  },
+  },
+  plugins: [],
+}
+
